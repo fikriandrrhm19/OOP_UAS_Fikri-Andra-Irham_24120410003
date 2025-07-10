d148e4d051a99ce9f053fa69d905f53081731b72
Fikri Andra Irham <fikri.andrhm@gmail.com>
2025-07-10 19:03:55 +0700

    Menambahkan kerangka dasar untuk semua kelas sesuai UML

---
+++ OOP_UAS1_Fikri Andra Irham_24120410003.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+class Person; class Player; class Coach; class Staff;
+class Team; class Club; class Stadium; class Sponsor;
+class Contract; class TrainingSession; class Match; class Season;
+
+class Person {};
+class Player : public Person {};
+class Coach : public Person {};
+class Staff : public Person {};
+class Stadium {};
+class Team {};
+class Club {};
+class Sponsor {};
+class Contract {};
+class TrainingSession {};
+class Match {};
+class Season {};
+
+int main() {
+    return 0;
+}
